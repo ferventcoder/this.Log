d1ec52497c893b0589ccc63b100d4a38ce30c10e
Rob Reynolds <ferventcoder@gmail.com>
2012-12-16 22:13:11 -0600

    point the nuget pack to a bin directory

--- build.ps1
+++ build.ps1
@@ -12,13 +12,19 @@ dir -r -include packages.config -exclude **\.git\** | %{
 $MsBuild="$($env:WINDIR)\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
 & $MsBuild $localDir\LoggingExtensions.sln /property:Configuration=Release
 
+
+$outputDir = join-path $localDir 'bin'
+if (!(test-path $outputDir)){
+  md $outputDir | out-null
+}
+
 dir -r -include *.csproj -exclude **\.git\**,**\*.Tests\** | %{  
   if ($_.FullName.ToLower().EndsWith(".sample.csproj") -or $_.FullName.ToLower().EndsWith(".sample.vbproj")) {
     $nuspec = join-path $_.Directory.Name $_.Name.Replace('csproj','nuspec').Replace('LoggingExtensions.','this.Log-')
     Write-Host "Building and packaging a nuget package for `'$nuspec'";
-    & $nugetExe pack $nuspec
+    & $nugetExe pack $nuspec -OutputDirectory "$outputDir"
   } else {
     Write-Host "Building and packaging a nuget package for `'$_.FullName'";
-    & $nugetExe pack $_.FullName -Prop Configuration=Release -Symbols
+    & $nugetExe pack $_.FullName -Prop Configuration=Release -Symbols -OutputDirectory "$outputDir"
   }
 }
