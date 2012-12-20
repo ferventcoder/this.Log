6c3f9bdf53051a99e63a5b5783615c7309f8fcdb
Rob Reynolds <ferventcoder@gmail.com>
2012-12-19 22:37:27 -0600

    Updated the version

--- SharedAssembly.cs
+++ SharedAssembly.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
