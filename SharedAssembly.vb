fe69bf46bbbcbb3f9c6ce7f1034371fd8968155b
Rob Reynolds <ferventcoder@gmail.com>
2012-12-19 22:14:26 -0600

    adding a vb shared assembly

---
+++ SharedAssembly.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyCompany("RealDimensions Software, LLC")> 
+<Assembly: AssemblyCopyright("Copyright ©  2012  RealDimensions Software, LLC")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: ComVisible(False)>
+
+<Assembly: AssemblyVersion("0.0.1.0")> 
+<Assembly: AssemblyFileVersion("0.0.1.0")> 
