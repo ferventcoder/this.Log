fef446b5e69f71864a778cd71bd9393da86afb0d
Rob Reynolds <ferventcoder@gmail.com>
2012-12-19 19:51:47 -0600

    Moving some files around. Adding a SharedAssembly.cs, adding it to all projects and removing shared assembly attributes from all of the assembly.cs files.

--- LoggingExtensions.log4net.Sample/Properties/AssemblyInfo.cs
+++ LoggingExtensions.log4net.Sample/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,30 +6,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("this.Log-log4net.Sample")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("RealDimensions Software, LLC")]
 [assembly: AssemblyProduct("this.Log-log4net.Sample")]
-[assembly: AssemblyCopyright("Copyright ©  2012 RealDimensions Software, LLC")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("81591123-786f-44c8-b3fa-2dc3ed0155ee")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: Guid("81591123-786f-44c8-b3fa-2dc3ed0155ee")]
